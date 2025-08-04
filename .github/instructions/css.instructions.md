f79675e6ea380edd74d373c88606af43264b00cd
Piotr Wysocki <piotr@goworks.dev>
2025-08-04 23:33:03 +0200

    app frame

---
+++ .github/instructions/css.instructions.md
@@ -0,0 +1,4 @@
+---
+applyTo: '**'
+---
+Tailwinf CSS 4.1 is zero configuration required by default use /app/globals.css
---
+++ app/layout.js
@@ -0,0 +1,27 @@
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
+
+const openSans = Open_Sans({
+  variable: "--font-open-sans",
+  subsets: ["latin", "latin-ext"],
+});
+
+export const metadata = {
+  title: {
+    template: '%s | Fargotex',
+    default: 'Fargotex',
+  },
+  description: '',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pl">
+      <body
+        className={`${openSans.variable} font-sans antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
